7da2d3b4bc99fe8903f0b5b13f0a4f2857f69716
prem <almaaux3@gmail.com>
2025-11-22 13:56:44 +0530

    commit

--- public/images/images.js
+++
@@ -1,9 +0,0 @@
-import logo_pic from './logo-1.webp'
-import ham_button from './hamburger-button.svg'
-import close_menu from './close-menu.svg'
-
-export const images = {
-    logo_pic,
-    ham_button,
-    close_menu
-}
